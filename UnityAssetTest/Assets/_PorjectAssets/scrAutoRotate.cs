53b35cb8bc5851ba24edac05c880b3cd1db87dfc
cliffordsakaki <cliffordsakaki@hotmail.com>
2017-11-20 20:27:16 +0900

    delete imported assets
    create objects for shader test
    
    create test shaders

---
+++ UnityAssetTest/Assets/_PorjectAssets/scrAutoRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrAutoRotate : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+		
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion rot = gameObject.transform.rotation;
+        Vector3 rotEuler = new Vector3 (0.0f, 1.0f, 0.0f);
+        rot *= Quaternion.Euler (rotEuler);
+        gameObject.transform.rotation = rot;
+    }
+}
